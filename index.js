2e77fd8e083a1e7641004623dc50678cd9d7c726
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-26 18:48:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(src, callback){
+  let ar = []
+  for (const i of src ){
+    ar.push(callback(i))
+  }
+  return ar
+}
+
+function reduce(src, callback, startingValue) {
+  let total;
+  if (startingValue) {
+    total = startingValue;
+    for (let i = 0; i < src.length; i++) {
+      total = callback(src[i], total);
+    }
+    return total;
+  } else {
+    total = src[0];
+    for (let i = 1; i < src.length; i++) {
+      total = callback(src[i], total);
+    }
+    return total;
+  }
+}
